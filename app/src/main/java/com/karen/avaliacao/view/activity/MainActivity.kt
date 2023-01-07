f2ace109c2b0899f6fb968f08551a611e80e674f
Karen Jully <karenjully.kr15@gmail.com>
2023-01-07 00:50:34 -0300

    initial layout

---
+++ app/src/main/java/com/karen/avaliacao/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.karen.avaliacao.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karen.avaliacao.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
