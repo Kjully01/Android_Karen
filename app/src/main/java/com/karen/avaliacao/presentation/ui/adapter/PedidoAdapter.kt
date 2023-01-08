2db697788ebfb68716a068f4828a81975c84aa71
Karen Jully <karenjully.kr15@gmail.com>
2023-01-08 17:32:39 -0300

    status do pedido

--- app/src/main/java/com/karen/avaliacao/presentation/ui/adapter/PedidoAdapter.kt
+++ app/src/main/java/com/karen/avaliacao/presentation/ui/adapter/PedidoAdapter.kt
@@ -2,6 +2,7 @@ package com.karen.avaliacao.presentation.ui.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.karen.avaliacao.databinding.ItemRecyclerPedidoBinding
@@ -12,7 +13,8 @@ class PedidoAdapter() : RecyclerView.Adapter<PedidoAdapter.ViewHolderPedido>() {
     private var pedidoList: MutableList<Pedido> = arrayListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderPedido {
-        val itemBinding = ItemRecyclerPedidoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val itemBinding =
+            ItemRecyclerPedidoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolderPedido(itemBinding)
     }
 
@@ -32,15 +34,38 @@ class PedidoAdapter() : RecyclerView.Adapter<PedidoAdapter.ViewHolderPedido>() {
     class ViewHolderPedido(private val binding: ItemRecyclerPedidoBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-            fun onBind(pedidoResponse: Pedido){
-                binding.apply {
-                    tvNumPedRca.text = pedidoResponse.numero_ped_Rca.toString()
-                    tvNumPedErp.text = pedidoResponse.numero_ped_erp.toString()
+        fun onBind(pedidoResponse: Pedido) {
+            binding.apply {
 
-                    val codName = pedidoResponse.codigoCliente + " - " + pedidoResponse.NOMECLIENTE
-                    tvClientInfo.text = codName
+                imgProcessamento.visibility = View.GONE
+                icRecusado.visibility = View.GONE
+                icPendente.visibility = View.GONE
+                icBloqueado.visibility = View.GONE
+                icLiberado.visibility = View.GONE
+                icMontado.visibility = View.GONE
+                icFaturado.visibility = View.GONE
+                icCancelado.visibility = View.GONE
+                icOrcamento.visibility = View.GONE
 
-                    tvStatus.text = pedidoResponse.status
+                tvNumPedRca.text = pedidoResponse.numero_ped_Rca.toString()
+                tvNumPedErp.text = pedidoResponse.numero_ped_erp.toString()
+
+                val codName = pedidoResponse.codigoCliente + " - " + pedidoResponse.NOMECLIENTE
+                tvClientInfo.text = codName
+
+                tvStatus.text = pedidoResponse.status
+
+                if (pedidoResponse.tipo == "ORCAMENTO") {
+                    icOrcamento.visibility = View.VISIBLE
+                } else {
+                    when (pedidoResponse.status) {
+                        "Em processamento" -> binding.imgProcessamento.visibility = View.VISIBLE
+                        "Pendente" -> binding.icPendente.visibility = View.VISIBLE
+                        else -> {
+                            binding.icLiberado.visibility = View.VISIBLE
+                        }
+                    }
+
                     //imgCritica
                     tvHora.text = pedidoResponse.data
                     //imgLegenda
@@ -48,6 +73,6 @@ class PedidoAdapter() : RecyclerView.Adapter<PedidoAdapter.ViewHolderPedido>() {
                 }
             }
 
+        }
     }
-
 }
