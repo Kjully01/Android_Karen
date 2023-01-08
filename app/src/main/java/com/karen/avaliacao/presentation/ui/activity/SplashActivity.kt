df6ef3b6c0e4cb172d326c1d0617e9db84ecbd1b
Karen Jully <karenjully.kr15@gmail.com>
2023-01-07 23:13:46 -0300

    SplashActivity

--- app/src/main/java/com/karen/avaliacao/presentation/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/karen/avaliacao/presentation/ui/activity/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.karen.avaliacao.presentation.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.karen.avaliacao.R
+import com.karen.avaliacao.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
